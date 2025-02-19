bce97a4a9795b2cc116f62a54ed2a58a9fb014d2
SamiOzkan <samiozkan5681@gmail.com>
2025-02-19 21:51:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    
+    // Tüm slaytları gizle
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
+    slides[slideIndex - 1].style.display = "block";
+    
+    // Her 3 saniyede bir slaytı değiştir
+    setTimeout(showSlides, 3000);
+}
